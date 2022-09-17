b438b3774b3c46a95357c315f51bc8c7186e43ec
camilobr89 <71193472+camilobr89@users.noreply.github.com>
2022-09-17 11:33:55 -0500

    Correccion

--- backend/authProject/settings.py
+++ backend/authProject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sks*=(o^!bv)-r6fg+jz0tv@dz_=1+0++(s+ibzk9w*-(=iv4e'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
